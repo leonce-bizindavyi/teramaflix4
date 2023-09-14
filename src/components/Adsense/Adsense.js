285a61d7ea3dffbf9c366b6dcd8fc8e1751845ab
leonce-bizindavyi <leonceb900@gmail.com>
2023-09-14 19:26:07 +0200

    first commit

--- src/components/Adsense/Adsense.js
+++ src/components/Adsense/Adsense.js
@@ -1,11 +1,29 @@
-import React, { useEffect } from 'react';
+import Head from 'next/head';
+import React, { useEffect,useRef } from 'react';
+import { useRouter } from 'next/router';
 
 const Adsense = () => {
-  useEffect(() => {
-    (window.adsbygoogle = window.adsbygoogle || []).push({});
-  }, []);
+  const router = useRouter();
+  const adsLoaded = useRef(false);
 
+  useEffect(() => {
+    const loadAd = () => {
+      if (typeof window !== "undefined" && window.adsbygoogle) {
+        window.adsbygoogle = window.adsbygoogle || [];
+        window.adsbygoogle.push({});
+        adsLoaded.current = true;
+      }
+    };
+  
+    if (router.query && !adsLoaded.current) {
+      setTimeout(loadAd, 0);
+    }
+  }, [router.query]);
   return (
+    <>
+    <Head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    </Head>
     <div>
       <ins
         className="adsbygoogle h-[50%]"
@@ -15,6 +33,7 @@ const Adsense = () => {
         data-full-width-responsive="true"
       ></ins>
     </div>
+    </>
   );
 };
 
