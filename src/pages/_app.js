a83dc8f870fe93247ec82a89d033c97288b9b960
leonce-bizindavyi <leonceb900@gmail.com>
2023-09-25 22:11:39 +0200

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,39 +8,41 @@ import '@/styles/fonts.css'
 import { SessionProvider } from '@/components/context/Auth'
 import Navbar from '@/components/Navs/Navbar'
 import Messagerie from '@/components/Messages/Messagerie'
-import {toast,ToastContainer } from 'react-toastify';
+import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useState } from 'react'
 import LinearIndeterminate from '@/components/Bar'
 import { LoadProvider } from '@/components/context/loading'
 
 export default function App({ Component, pageProps }) {
-  if(Component.getLayout){
-      return Component.getLayout(<Component {...pageProps} />)
-    }
-  const [blur , setBlur] = useState(false)
+  const [blur, setBlur] = useState(false)
+
+  if (Component.getLayout) {
+    return Component.getLayout(<Component {...pageProps} />)
+  }
+
   const sideAllOpened = (state) => {
     setBlur(state)
   }
+
   return (
     <SessionProvider>
       <LoadProvider>
-       <div className="font-quicksand">
-          <div className="wrapper relative w-full h-full bg-gray-100   pt-1 overflow-x-hidden ">
-          <Navbar sideAllOpened = {sideAllOpened} />
-        <div className={`Acceuilcontainer ${blur? 'blur': {}}  w-full  justify-center items-center  bg-gray-100 flex flex-col h-full `}>
-            <div className={`container w-[100%] h-[100%] lg:px-6    bg-white lg:p-4 lg:rounded  flex flex-col justify-center`}>
-              <LinearIndeterminate />
-            <Component {...pageProps} />
-            <Messagerie />
-          </div>
+        <div className="font-quicksand">
+          <div className="wrapper relative w-full h-full bg-gray-100 pt-1 overflow-x-hidden ">
+            <Navbar sideAllOpened={sideAllOpened} />
+            <div className={`Acceuilcontainer ${blur ? 'blur' : ''} w-full justify-center items-center bg-gray-100 flex flex-col h-full `}>
+              <div className={`container w-[100%] h-[100%] lg:px-6 bg-white lg:p-4 lg:rounded  flex flex-col justify-center`}>
+                <LinearIndeterminate />
+                <Component {...pageProps} />
+                <Messagerie />
+              </div>
+            </div>
           </div>
         </div>
-       </div>
-      
-      <ToastContainer />
+
+        <ToastContainer />
       </LoadProvider>
     </SessionProvider>
-  
   )
 }
